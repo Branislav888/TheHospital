4924c65996389c64524bd1e2b2692cd2314e0cfd
alica <alicalastikova12@gmail.com>
2026-04-09 11:18:59 +0200

    jjjjj

--- script.js
+++ script.js
@@ -1,13 +1,29 @@
-const logo = document.querySelector(".logo");
+const audio = document.getElementById('cinematicAudio');
+const muteBtn = document.getElementById('muteMasterBtn');
+const icon = document.getElementById('audioIcon');
+
+let muted = false;
+
+audio.loop = true;
+
+audio.play().catch(() => {
+    muted = true;
+    audio.muted = true;
+    icon.src = "Images/mute.png";
+});
+
+muteBtn.onclick = () => {
+    muted = !muted;
+    audio.muted = muted;
+    icon.src = muted ? "Images/mute.png" : "Images/unmute.png";
+    if (!muted && audio.paused) audio.play();
+};
 
 function animateLogo() {
+    const logo = document.querySelector('.logo');
     let time = Date.now() / 800;
     let move = Math.sin(time) * 8;
-
     logo.style.transform = `translateY(${move}px)`;
-
     requestAnimationFrame(animateLogo);
 }
-
 animateLogo();
-
